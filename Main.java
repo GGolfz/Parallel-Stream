badb3764588ef9f045e51605564791af1fc56c18
GGolfz <wisarutgolf11@gmail.com>
2021-11-01 10:31:18 +0700

    update: run :sparkles:

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
   static final String INPUT_SMALL = "5000 BT Records.csv";
@@ -5,8 +6,20 @@ public class Main {
   static final double CORE = 4;
 
   public static void main(String[] args) {
-    // runQ1();
-    runQ2();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the question (Press 1 for Q1, Press 2 for Q2): ");
+    int question = sc.nextInt();
+    if(question == 1) {
+      runQ1();
+    } else if(question == 2) {
+      System.out.println("Enter the task (Press 1 for Task 2.1, Press 2 for Task 2.2): ");
+      int task = sc.nextInt();
+      if(task == 1) {
+        runQ2_1();
+      } else if(task == 2) {
+        runQ2_2();
+      }
+    } 
   }
 
   public static void runQ1() {
@@ -40,13 +53,13 @@ public class Main {
     System.out.println("============================================================================");
   }
 
-  public static void runQ2() {
+  public static void runQ2_1() {
     double ts = 0;
     double tp = 0;
     double speedup = 0;
     double efficiency = 0;
-    System.out.println("===================== QUESTION 2 ("+INPUT_SMALL+") =====================");
     System.out.println("================================== TASK 1 ==================================");
+    System.out.println("===================== QUESTION 2 ("+INPUT_SMALL+") =====================");
     System.out.println("================================= SEQUENCE =================================");
     ts = Q2.runT1(INPUT_SMALL, Type.SEQUENCE);
     System.out.println("================================= PARALLEL =================================");
@@ -58,7 +71,27 @@ public class Main {
     System.out.println("Speed Up: " + speedup + " times faster");
     System.out.println("Efficiency: " + efficiency + " %");
     System.out.println("============================================================================");
+    System.out.println("===================== QUESTION 2 ("+INPUT_LARGE+") ==================");
+    System.out.println("================================= SEQUENCE =================================");
+    ts = Q2.runT1(INPUT_LARGE, Type.SEQUENCE);
+    System.out.println("================================= PARALLEL =================================");
+    tp = Q2.runT1(INPUT_LARGE, Type.PARALLEL);
+    System.out.println("Sequence: " + ts + " ms");
+    System.out.println("Parallel: " + tp + " ms");
+    speedup = ts/tp;
+    efficiency = (speedup*100)/CORE;
+    System.out.println("Speed Up: " + speedup + " times faster");
+    System.out.println("Efficiency: " + efficiency + " %");
+    System.out.println("============================================================================");
+  }
+
+  public static void runQ2_2() {
+    double ts = 0;
+    double tp = 0;
+    double speedup = 0;
+    double efficiency = 0;
     System.out.println("================================== TASK 2 ==================================");
+    System.out.println("===================== QUESTION 2 ("+INPUT_SMALL+") =====================");
     System.out.println("================================= SEQUENCE =================================");
     ts = Q2.runT2(INPUT_SMALL, Type.SEQUENCE);
     System.out.println("================================= PARALLEL =================================");
@@ -70,17 +103,17 @@ public class Main {
     System.out.println("Speed Up: " + speedup + " times faster");
     System.out.println("Efficiency: " + efficiency + " %");
     System.out.println("============================================================================");
-    // System.out.println("===================== QUESTION 2 ("+INPUT_LARGE+") ==================");
-    // System.out.println("================================= SEQUENCE =================================");
-    // ts = Q2.run(INPUT_LARGE, Type.SEQUENCE);
-    // System.out.println("================================= PARALLEL =================================");
-    // tp = Q2.run(INPUT_LARGE, Type.PARALLEL);
-    // System.out.println("Sequence: " + ts + " ms");
-    // System.out.println("Parallel: " + tp + " ms");
-    // speedup = ts/tp;
-    // efficiency = (speedup*100)/CORE;
-    // System.out.println("Speed Up: " + speedup + " times faster");
-    // System.out.println("Efficiency: " + efficiency + " %");
-    // System.out.println("============================================================================");
+    System.out.println("===================== QUESTION 2 ("+INPUT_LARGE+") ==================");
+    System.out.println("================================= SEQUENCE =================================");
+    ts = Q2.runT2(INPUT_LARGE, Type.SEQUENCE);
+    System.out.println("================================= PARALLEL =================================");
+    tp = Q2.runT2(INPUT_LARGE, Type.PARALLEL);
+    System.out.println("Sequence: " + ts + " ms");
+    System.out.println("Parallel: " + tp + " ms");
+    speedup = ts/tp;
+    efficiency = (speedup*100)/CORE;
+    System.out.println("Speed Up: " + speedup + " times faster");
+    System.out.println("Efficiency: " + efficiency + " %");
+    System.out.println("============================================================================");
   }
 }
